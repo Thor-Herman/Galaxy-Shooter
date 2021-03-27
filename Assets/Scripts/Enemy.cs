015517bb4cdb685d82c465a0b0cddeaf74aab76c
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-03-27 20:16:18 +0100

    Add OnDeath trigger to Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,14 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private float _speed = 4.0f;
     private Player _player;
+    private Animator _animator;
 
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        _animator = GetComponent<Animator>();
+        if (_player == null) Debug.LogError("Player is null");
+        if (_animator == null) Debug.LogError("Player is null");
     }
 
     // Update is called once per frame
@@ -29,11 +33,13 @@ public class Enemy : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             if (_player != null) _player.DamagePlayer(1);
+            _animator.SetTrigger("OnEnemyDeath");
             Destroy(this.gameObject);
         }
         else if (other.gameObject.CompareTag("Laser"))
         {
             if (_player != null) _player.IncrementScore(10);
+            _animator.SetTrigger("OnEnemyDeath");
             Destroy(other.gameObject);
             Destroy(this.gameObject);
         }
