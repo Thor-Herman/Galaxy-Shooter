b2b280396dfe588a122f986f7c7dedd9aa5dfa4b
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-03-31 20:37:44 +0200

    Destroy object only after animation plays

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,21 +8,25 @@ public class Enemy : MonoBehaviour
     private float _speed = 4.0f;
     private Player _player;
     private Animator _animator;
+    private bool _deathAnimPlaying = false;
+    private const float _animationDuration = 2.633f;
 
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
         _animator = GetComponent<Animator>();
         if (_player == null) Debug.LogError("Player is null");
-        if (_animator == null) Debug.LogError("Player is null");
+        if (_animator == null) Debug.LogError("Animator is null");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.down * Time.deltaTime * _speed);
-        if (transform.position.y < -5f)
+        bool hasLeftGameFrame = transform.position.y < -5f;
+        if (hasLeftGameFrame)
         {
+            if (_deathAnimPlaying) Destroy(this.gameObject); // Finish animation early. 
             float newXPos = Random.Range(-8.5f, 8.5f);
             transform.position = new Vector3(newXPos, 7, 0);
         }
@@ -34,14 +38,21 @@ public class Enemy : MonoBehaviour
         {
             if (_player != null) _player.DamagePlayer(1);
             _animator.SetTrigger("OnEnemyDeath");
-            Destroy(this.gameObject);
+            OnDeath();
         }
         else if (other.gameObject.CompareTag("Laser"))
         {
             if (_player != null) _player.IncrementScore(10);
-            _animator.SetTrigger("OnEnemyDeath");
+            OnDeath();
             Destroy(other.gameObject);
-            Destroy(this.gameObject);
         }
+    }
+
+    void OnDeath()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        _animator.SetTrigger("OnEnemyDeath");
+        _deathAnimPlaying = true;
+        Destroy(this.gameObject, _animationDuration); 
     }
 }
