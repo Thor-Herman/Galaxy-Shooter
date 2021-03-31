07bfa1962ec337329af393eaf9cf16043d59d029
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-03-31 22:09:44 +0200

    Add Asteroid rotation and explosion

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private const float _rotationSpeed = 15f;
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    private SpawnManager _spawnManager;
+
+    private void Start() {
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        if (_spawnManager == null) Debug.LogError("SpawnManager is null");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
+        if (_explosionPrefab == null) Debug.LogError("Explosion prefab is null");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Laser")) {
+            Instantiate(_explosionPrefab, this.gameObject.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            _spawnManager.StartSpawning();
+            Destroy(this.gameObject, 0.25f);
+        }
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    private const float _animationDuration = 2.633f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, _animationDuration);
+    }
+}
