78858dd84106575c0f0fcf3751b6802d2902e9b9
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-04-03 20:50:06 +0200

    Add gradual decrease in difficulty
    Expand bottom window size a bit

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -1,6 +1,6 @@
 public static class GameConstants {
     public const float WINDOW_TOP_POS = 10.5f;
-    public const float WINDOW_BOTTOM_POS = -5f;
+    public const float WINDOW_BOTTOM_POS = -5.5f;
     public const float WINDOW_HALF_WIDTH = 13.75f;
     public const float ENEMY_X_LIMIT = 11f;
     // public const float WINDOW_LEFT_POS =
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,32 +12,32 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject[] _powerUps;
     [SerializeField]
-    private float _minWaitTimePowerUp;
-    [SerializeField]
-    private float _maxWaitTimePowerUp;
+    private float _minWaitTimePowerUp, _maxWaitTimePowerUp, _spawnRoutinesDelay = 3.0f;
+    private float _enemyRespawnTime = 5.0f;
     private bool _stopSpawning = false;
 
     public void StartSpawning()
     {
         StartCoroutine(SpawnEnemyRoutine());
         StartCoroutine(SpawnPowerUpRoutine());
+        StartCoroutine(IncreaseDifficultyRoutine());
     }
 
     IEnumerator SpawnEnemyRoutine()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(_spawnRoutinesDelay);
         while (!_stopSpawning)
         {
             Vector3 posToSpawn = GetPosToSpawn();
             GameObject spawnedEnemy = Instantiate(_enemy, posToSpawn, Quaternion.identity);
             spawnedEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(_enemyRespawnTime);
         }
     }
 
     IEnumerator SpawnPowerUpRoutine()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(_spawnRoutinesDelay);
         while (!_stopSpawning)
         {
             Vector3 posToSpawn = GetPosToSpawn();
@@ -47,6 +47,14 @@ public class SpawnManager : MonoBehaviour
             float waitTime = Random.Range(_minWaitTimePowerUp, _maxWaitTimePowerUp);
             yield return new WaitForSeconds(waitTime);
         }
+    }
+
+    IEnumerator IncreaseDifficultyRoutine() {
+        yield return new WaitForSeconds(_spawnRoutinesDelay);
+        while (_enemyRespawnTime >= 0.5f && !_stopSpawning) {
+            yield return new WaitForSeconds(7);
+            _enemyRespawnTime -= 0.5f;
+        }   
     }
 
     Vector3 GetPosToSpawn()
